079119f38fa1a4415864144250002ecccd4e064d
Hector Salomon Aquino <hector.aquino@ues.edu.sv>
2022-02-19 11:41:37 -0600

    actividad ardora
    actividad ardora

---
+++ ORD_resources/js/ardoraOrdenaCFG.js
@@ -0,0 +1,29 @@
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=1; attempts=0; attemptsMax=1;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Bien hecho"; messageTime="Se termino el tiempo"; messageError="Lo siento, respuesta equivocada"; messageErrorG="Lo siento, respuesta equivocada"; messageAttempts="Se terminaron los intentos"; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="T1JE"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["RG8gbWUgYSBmYXZvciwgc21pbGU=","","","",""];imaW=["smile03.png","","","",""];queW=["be happy","","","",""]; c=[20,0,0,0,0];
+var auW=["MQ==","","","",""];
+var actMaxWidth="600"; actMaxHeight="400";indexG=0;profG=0;dirMedia="ORD_resources/media/"; textBNext="";
+var wordsG=[];imageW=[];questionW=[];audioW=[];cG=[];
